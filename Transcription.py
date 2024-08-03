22c0592a311783fa80c49fcece505651ae01e154
IvannaGotsova <ivannagotsova@outlook.com>
2024-08-03 20:37:52 +0300

    Transcription

--- Transcription.py
+++ Transcription.py
@@ -6,3 +6,6 @@ transcription_sequence = my_sequence.transcribe()
 print(my_sequence)
 print(transcription_sequence)
 
+transcription_sequence = my_sequence.back_transcribe()
+
+print(transcription_sequence)
