46d01eb5743eb138f55082c68335973ba3a9a89a
IvannaGotsova <ivannagotsova@outlook.com>
2025-02-12 21:12:31 +0200

    FASTA

---
+++ FASTA.py
@@ -0,0 +1,15 @@
+
+fileExample = 'file.fasta'
+
+with open(fileExample, 'w') as f:
+    f.write('AAA')
+    f.write('\n')
+    f.write('GGG')
+    f.write('\n')
+    f.write('CCC')
+    f.write('\n')
+    f.write('TTT')
+    f.write('\n')
+    f.write('UUU')
+
+
